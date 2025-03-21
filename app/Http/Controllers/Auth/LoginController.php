46eb2bc0ce543398b5bb1b2b39dcb178db89148e
lavivah amelia <e41230642@student.polije.ac.id>
2025-03-21 10:44:00 +0700

    acara 11

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -37,4 +39,26 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    public function login(Request $request) {
+        $this->validate($request, [
+            'username' =>'required|string',
+            'password' =>'required|string|min:6',
+        ]);
+    
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+    
+        $login = [
+            $loginType => $request->username,
+            'password' => $request->password
+        ];
+    
+        if (auth()->attempt($login)) {
+            return redirect()->route('home');
+        }
+    
+        return redirect()->route('login')->with(['error' => 'email/password salah!']);
+    }
+    
 }
+
